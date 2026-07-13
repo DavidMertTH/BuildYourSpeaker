9f2d10103109869cbc2d05b1a34e624f29494a79
dmertens <david_tim_luca.mertens@smail.th-koeln.de>
2026-07-13 15:30:28 +0200

    feat: add project undo and redo

--- src/svelte/workspace/AppToolbar.svelte
+++ src/svelte/workspace/AppToolbar.svelte
@@ -1,11 +1,13 @@
 <script>
   import FileMenu from "./toolbar/FileMenu.svelte";
   import GraphMenu from "./toolbar/GraphMenu.svelte";
+  import HistoryControls from "./toolbar/HistoryControls.svelte";
   import ThemeToggle from "./toolbar/ThemeToggle.svelte";
 </script>
 
 <header class="app-toolbar">
   <FileMenu />
+  <HistoryControls />
   <GraphMenu />
   <ThemeToggle />
 </header>
